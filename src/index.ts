1f4eb38f3203830828c803e260f127031ed0a83a
Scott Tolinski <scott.tolinski@gmail.com>
2021-08-11 12:02:07 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// Changes css class based on the container's size.
+export const container = (
+  node: HTMLElement,
+  sizes: { [k: string]: number } = {
+    "sec-small": 0,
+    "sec-medium": 200,
+    "sec-large": 400,
+  }
+): { destroy: () => void } => {
+  const ro = new ResizeObserver(() => {
+    const nodeSize = node.getBoundingClientRect().width;
+    let activeClass = "";
+    for (const property in sizes) {
+      if (nodeSize > sizes[property]) activeClass = property;
+      node.classList.remove(property);
+    }
+    node.classList.add(activeClass);
+  });
+
+  ro.observe(node);
+
+  return {
+    destroy() {
+      ro.disconnect();
+    },
+  };
+};
