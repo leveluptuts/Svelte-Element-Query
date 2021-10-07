b72830c388ec05ba1822254e862e532b145feff7
Scott Tolinski <scott.tolinski@gmail.com>
2021-10-07 13:34:06 -0600

    adds dist to repo

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export declare const container: (node: HTMLElement, sizes?: {
+    [k: string]: number;
+}) => {
+    destroy: () => void;
+};
---
+++ dist/index.js
@@ -0,0 +1,23 @@
+// Changes css class based on the container's size.
+export const container = (node, sizes = {
+    "sec-small": 0,
+    "sec-medium": 200,
+    "sec-large": 400,
+}) => {
+    const ro = new ResizeObserver(() => {
+        const nodeSize = node.getBoundingClientRect().width;
+        let activeClass = "";
+        for (const property in sizes) {
+            if (nodeSize > sizes[property])
+                activeClass = property;
+            node.classList.remove(property);
+        }
+        node.classList.add(activeClass);
+    });
+    ro.observe(node);
+    return {
+        destroy() {
+            ro.disconnect();
+        },
+    };
+};
